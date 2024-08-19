87a4ef6ae7edca604c20fd4ade5f4ce69c45f3a0
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-08-19 18:22:35 +0200

    sistemato riempimento seeder

--- database/migrations/2024_08_01_143641_add_type_id_projects_table.php
+++ database/migrations/2024_08_01_143641_add_type_id_projects_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigInteger('type_id');
-            $table->foreign('type_id')->references('id')->on('types');
+            $table->unsignedBigInteger('type_id')->nullable();
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null')->onUpdate('cascade');
         });
     }
 
