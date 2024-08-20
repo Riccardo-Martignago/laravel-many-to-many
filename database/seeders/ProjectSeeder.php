9a3236129ec9a37cb5e47d065439303260f82a5b
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-08-20 15:15:06 +0200

    Aggiunto type id su projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -14,8 +15,10 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        $types = Type::all()->pluck('id');
         for ($i = 0; $i < 20; $i++){
             $projects = new Project();
+            $projects->type_id = $faker->randomElement($types);
             $projects->name = $faker->word();
             $projects->description = $faker->paragraph(4);
             $projects->save();
